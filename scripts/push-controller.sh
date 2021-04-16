7a7460cb9adbde7ce60b3fe8bf7a8d37cfc31558
covariance <covariance@yandex-team.ru>
2021-04-16 16:38:40 +0300

    Script fixed

--- scripts/push-controller.sh
+++ scripts/push-controller.sh
@@ -3,5 +3,5 @@
 REGISTRY="$2"
 
 make docker-build CONNECTOR="$1"
-docker tag controller-"$1" "$REGISTRY"/"$1"-controller:latest
-docker push "$REGISTRY"/controller-"$1":latest
+docker tag "$1"-controller "$REGISTRY"/"$1"-controller:latest
+docker push "$REGISTRY"/"$1"-controller:latest
