1474e27f2804e18a6904a20c6f7d8ec3e7f1795d
covariance <covariance@yandex-team.ru>
2021-04-21 12:59:54 +0300

    Added fake logr implementation

--- testing/logr-fake/fake_logr.go
+++ testing/logr-fake/fake_logr.go
@@ -3,12 +3,25 @@
 
 package logr_fake
 
-import "github.com/go-logr/logr"
+import (
+	"github.com/go-logr/logr"
+	"testing"
+)
 
-type FakeLogger struct {}
+type FakeLogger struct {
+	Level int
+	Name string
+	KV []interface{}
+	T *testing.T
+}
 
-func NewFakeLogger() logr.Logger {
-	return FakeLogger{}
+func NewFakeLogger(t *testing.T) logr.Logger {
+	return FakeLogger{
+		Level: 0,
+		Name: "",
+		KV: []interface{}{},
+		T : t,
+	}
 }
 
 // Enabled tests whether this Logger is enabled.  For example, commandline
@@ -24,7 +37,12 @@ func (r FakeLogger) Enabled() bool {
 // the log line.  The key/value pairs can then be used to add additional
 // variable information.  The key/value pairs should alternate string
 // keys and arbitrary values.
-func (r FakeLogger) Info(msg string, keysAndValues ...interface{}) {}
+func (r FakeLogger) Info(msg string, keysAndValues ...interface{}) {
+	r.T.Log("level", r.Level, "info:")
+	r.T.Log(r.KV...)
+	r.T.Log(msg)
+	r.T.Log(keysAndValues...)
+}
 
 // Error logs an error, with the given message and key/value pairs as context.
 // It functions similarly to calling Info with the "error" named value, but may
@@ -34,20 +52,36 @@ func (r FakeLogger) Info(msg string, keysAndValues ...interface{}) {}
 // The msg field should be used to add context to any underlying error,
 // while the err field should be used to attach the actual error that
 // triggered this log line, if present.
-func (r FakeLogger) Error(err error, msg string, keysAndValues ...interface{}) {}
+func (r FakeLogger) Error(err error, msg string, keysAndValues ...interface{}) {
+	r.T.Log("level", r.Level, "error:")
+	r.T.Log(r.KV...)
+	r.T.Log(err)
+	r.T.Log(msg)
+	r.T.Log(keysAndValues...)
+}
 
 // V returns an Logger value for a specific verbosity level, relative to
 // this Logger.  In other words, V values are additive.  V higher verbosity
 // level means a log message is less important.  It's illegal to pass a log
 // level less than zero.
 func (r FakeLogger) V(level int) logr.Logger {
-	return r
+	return FakeLogger{
+		Level: r.Level + level,
+		Name: r.Name,
+		KV: r.KV,
+		T:     r.T,
+	}
 }
 
 // WithValues adds some key-value pairs of context to a logger.
 // See Info for documentation on how key/value pairs work.
 func (r FakeLogger) WithValues(keysAndValues ...interface{}) logr.Logger {
-	return r
+	return FakeLogger{
+		Level: r.Level,
+		Name: r.Name,
+		KV:    append(r.KV, keysAndValues...),
+		T:     r.T,
+	}
 }
 
 // WithName adds a new element to the logger's name.
@@ -56,5 +90,10 @@ func (r FakeLogger) WithValues(keysAndValues ...interface{}) logr.Logger {
 // that name segments contain only letters, digits, and hyphens
 // (see the package documentation for more information).
 func (r FakeLogger) WithName(name string) logr.Logger {
-	return r
+	return FakeLogger{
+		Level: r.Level,
+		Name:  r.Name + name,
+		KV:    r.KV,
+		T:     r.T,
+	}
 }
