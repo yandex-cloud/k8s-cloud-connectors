448f55aa9406eb7b2b5e5e2678f5a3291dea9fe2
covariance <covariance@yandex-team.ru>
2021-04-27 20:11:30 +0300

    EndpointProvider test skeleton added

--- connectors/ycr/controllers/phases/endpoint_provider_test.go
+++ connectors/ycr/controllers/phases/endpoint_provider_test.go
@@ -6,13 +6,87 @@ package phases
 import "testing"
 
 func TestEndpointProviderIsUpdated(t *testing.T) {
+	t.Run("is updated on configmap existence", func(t *testing.T) {
+		// Arrange
 
+		// Act
+
+		// Assert
+	})
+
+	t.Run("is updated on many configmap existence", func(t *testing.T) {
+		// Arrange
+
+		// Act
+
+		// Assert
+	})
+
+	t.Run("is not updated on empty cloud", func(t *testing.T) {
+		// Arrange
+
+		// Act
+
+		// Assert
+	})
+
+	t.Run("is not updated on other objects existence", func(t *testing.T) {
+		// Arrange
+
+		// Act
+
+		// Assert
+	})
 }
 
 func TestEndpointProviderUpdate(t *testing.T) {
+	t.Run("update on empty cloud creates configmap", func(t *testing.T) {
+		// Arrange
 
+		// Act
+
+		// Assert
+	})
+
+	t.Run("update on non-empty cloud creates configmap", func(t *testing.T) {
+		// Arrange
+
+		// Act
+
+		// Assert
+	})
 }
 
 func TestEndpointProviderCleanup(t *testing.T) {
+	t.Run("cleanup on empty cloud does nothing", func(t *testing.T) {
+		// Arrange
 
+		// Act
+
+		// Assert
+	})
+
+	t.Run("cleanup on cloud with other configmaps does nothing", func(t *testing.T) {
+		// Arrange
+
+		// Act
+
+		// Assert
+	})
+
+	t.Run("cleanup on cloud with this and other configmaps deletes this configmap", func(t *testing.T) {
+		// Arrange
+
+		// Act
+
+		// Assert
+	})
+
+	t.Run("cleanup on cloud with this configmap deletes this configmap", func(t *testing.T) {
+		// Arrange
+
+		// Act
+
+		// Assert
+	})
 }
